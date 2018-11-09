76a8c1319113144dfb97c61e4c8e8111b5146db9
mmilic1994 <33220095+mmilic1994@users.noreply.github.com>
2018-11-09 14:47:11 +0100

    arpad kajari 10th

--- src/drop-down/drop-down.jsx
+++ src/drop-down/drop-down.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { timingSafeEqual } from 'crypto';
 
 const destinations = ['Valencia', 'Barcelona', 'Madrid', 'Milan', 'Athens'];
 const origins = ['Prague', 'Berlin', 'Warsaw', 'Pardubice'];
@@ -9,20 +10,24 @@ export default class DropDown extends React.Component {
 
         this.state = {
             origin: "Prague",
-            destination: "London"
+            destination: "London",
+            direct : 0
         };
     }
 
     action = (e) => {
         console.log("origin:" + document.getElementById('origin').value)
         console.log("destination:" + document.getElementById('destination').value)
-        
+        console.log(document.getElementById('direct_flights').checked);
 
         this.props.action({
             origin: document.getElementById('origin').value,
-            destination: document.getElementById('destination').value
-        })
+            destination: document.getElementById('destination').value,
+            direct: (document.getElementById('direct_flights').checked ? 1 : 0)  
+        });
     }
+
+
 
     render() {
         return (
@@ -42,8 +47,10 @@ export default class DropDown extends React.Component {
                     )
                     }  
                 </select>
-                <br />
+                <br /> 
+                <input type="checkbox" name="checkbox" id="direct_flights"/>direct flights only <br></br>
                 <button onClick={this.action}>Search connections!</button>
+                
             </>
         )
     }
