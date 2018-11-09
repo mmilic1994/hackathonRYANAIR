5ab2e79fb49631b3cdd4a46a31eaae6d0ac88987
Tomi <tomi.holstila@gmail.com>
2018-11-09 11:06:38 +0100

    simple loading indicator

--- src/flight_list/flight_list.jsx
+++ src/flight_list/flight_list.jsx
@@ -7,24 +7,29 @@ export default class FlightList extends React.Component {
     super(props);
 
     this.state = {
-      flights: []
+      flights: [],
+      isLoading: false
     }
 
   }
 
   componentDidMount() {
+    this.setState({isLoading: true})
     fetch('https://api.skypicker.com/flights?flyFrom=PRG&to=LGW&dateFrom=16/11/2018&dateTo=19/11/2018&partner=picky&direct_flights=1')
       .then(resp => resp.json())
       .then(json => {
         this.setState({
-          flights: json.data
+          flights: json.data,
+          isLoading: false
         });
       });
       
   }
   
   render() {
-    
+    if (this.state.isLoading == true) {
+      return ("loading...")
+    }
     return (
       <>
        <div className="flight_list">
